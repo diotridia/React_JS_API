19ab634fcf6d6431e6c06328d6d447e5039e2f19
diotridia <dio.tridia@gmail.com>
2019-05-20 13:33:14 +0700

    first commit

--- src/Australia.js
+++ src/Australia.js
@@ -11,14 +11,16 @@ class Australia extends Component {
   }
 
   componentDidMount() {
-      fetch("https://randomuser.me/api/?results=10&nat=id")
+      fetch("https://randomuser.me/api/?results=10&nat=AU")
         .then(res => res.json())
         .then(parsedJSON => parsedJSON.results.map(data => (
           {
             id: `${data.id.name}`,
             firstName: `${data.name.first}`,
             lastName: `${data.name.last}`,
-            location: `${data.location.state}, ${data.nat}`,
+            email: `${data.email}`,
+            username: `${data.login.username}`,
+            password: `${data.login.password}`,
             thumbnail: `${data.picture.large}`,
 
           }
@@ -37,14 +39,18 @@ class Australia extends Component {
             <h2>Random User</h2>
             {
               items.length > 0 ? items.map(item => {
-              const {id, firstName, lastName, location, thumbnail} = item;
+              const {id, firstName, lastName,email,username,password,thumbnail,} = item;
                return (
 
                <div key={id} className="bgCircle">
                <center><img src={thumbnail} alt={firstName} className="circle"/> </center><br />
                <div className="ctr">
                   {firstName} {lastName}<br />
-                  {location}
+                  {email}
+                  <br/>
+                  {username}
+                  <br/>
+                  {password}
                 </div>
 
               </div>
